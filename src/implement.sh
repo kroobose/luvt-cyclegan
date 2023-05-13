b109c78a55d320397cd9356ae47d24c1c5f609ac
NaKa <mmm.sss3838@gmail.com>
2023-05-13 15:30:31 +0900

    fixed shell script

--- src/implement.sh
+++ src/implement.sh
@@ -4,5 +4,5 @@ n_epochs=2 # 最大のepoch数
 n_epochs_decay=0
 iter_freq=2000 # どの程度の間隔の反復数で保存，テストするか．
 
-# python train.py --dataroot ./datasets/luvt --name $name --model cycle_gan --n_epochs $n_epochs --n_epochs_decay $n_epochs_decay --save_latest_freq $iter_freq --save_by_iter && \
+python train.py --dataroot ./datasets/luvt --name $name --model cycle_gan --n_epochs $n_epochs --n_epochs_decay $n_epochs_decay --save_latest_freq $iter_freq --save_by_iter && \
 python test.py --dataroot ./datasets/luvt --name $name --model cycle_gan --phase train --num_test $test --no_dropout --load_iter $iter_freq
